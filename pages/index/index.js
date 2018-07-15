360adcf10bf18d6ce8c7ad93364c2cf87f2a41b1
GeYaoFullStacker <alexgecontrol@qq.com>
2018-07-15 18:08:48 +0800

    Finish weather forecast section.

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,8 +8,76 @@ Page(
     data: {
       now: {
         temp: 12,
-        weather: "晴"
+        weather: "晴",
+        background: "/resources/images/weather/sunny-bg.png"
+      },
+      forecasts: []
+    },
+    // 功能函数: 更新当前天气
+    setNow(now) {
+      // 导航栏颜色设置:
+      const weatherNavBarColor = {
+        'sunny': '#cbeefd',
+        'cloudy': '#deeef6',
+        'overcast': '#c6ced2',
+        'lightrain': '#bdd5e1',
+        'heavyrain': '#c5ccd0',
+        'snow': '#aae1fc'
+      };
+      // 天气编码解析:
+      const weatherCookbook = {
+        'sunny': '晴天',
+        'cloudy': '多云',
+        'overcast': '阴',
+        'lightrain': '小雨',
+        'heavyrain': '大雨',
+        'snow': '雪'
       }
+
+      // 解析当前天气:
+      let temp = now.temp;
+      let weather = now.weather;
+      let background = "/resources/images/weather/" + weather + "-bg.png";
+
+      // 更新导航栏颜色:
+      wx.setNavigationBarColor(
+        {
+          frontColor: '#ffffff',
+          backgroundColor: weatherNavBarColor[weather]
+        }
+      );
+      // 更新当前天气:
+      this.setData(
+        {
+          now: {
+            temp: temp,
+            weather: weatherCookbook[weather],
+            background: background
+          }
+        }
+      );
+    },
+    // 功能函数: 更新天气预报
+    setForecasts(forecast) {
+      // 解析天气预报:
+      let nowHour = new Date().getHours();
+      let forecasts = [];
+      for (let i = 0; i < forecast.length; ++i) {
+        forecasts.push(
+          {
+            time: ((nowHour + 3 * i) % 24) + "时",
+            icon: "/resources/images/forecast/" + forecast[i].weather + "-icon.png",
+            temp: forecast[i].temp
+          }
+        )
+      }
+      forecasts[0].time = "现在";
+      // 更新当前天气:
+      this.setData(
+        {
+          forecasts: forecasts 
+        }
+      );
     },
     // 功能函数: 更新天气显示
     getNow(completeCallback) {
@@ -23,48 +91,12 @@ Page(
             'content-type': 'application/json'
           },
           success: res => {
-            // 导航栏颜色设置:
-            const weatherNavBarColor = {
-              'sunny': '#cbeefd',
-              'cloudy': '#deeef6',
-              'overcast': '#c6ced2',
-              'lightrain': '#bdd5e1',
-              'heavyrain': '#c5ccd0',
-              'snow': '#aae1fc'
-            };
-            // 天气编码解析:
-            const weatherCookbook = {
-              'sunny': '晴天',
-              'cloudy': '多云',
-              'overcast': '阴',
-              'lightrain': '小雨',
-              'heavyrain': '大雨',
-              'snow': '雪'
-            }
-
             // 服务器返回数据:
-            var weatherInfo = res.data.result;
-
-            // 解析当前天气:
-            var temp = weatherInfo.now.temp;
-            var weather = weatherInfo.now.weather;
-
-            // 更新导航栏颜色:
-            wx.setNavigationBarColor(
-              {
-                frontColor: '#ffffff',
-                backgroundColor: weatherNavBarColor[weather]
-              }
-            );
+            let result = res.data.result;
             // 更新当前天气:
-            this.setData(
-              {
-                now: {
-                  temp: temp,
-                  weather: weatherCookbook[weather]
-                }
-              }
-            );
+            this.setNow(result.now);
+            // 更新天气预报:
+            this.setForecasts(result.forecast);
           },
           complete: () => {
             completeCallback && completeCallback();
